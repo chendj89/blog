bd4d0a619e668137b910e4f88f8fc179f800c647
chendj89 <chendj89@hotmail.com>
2022-11-12 19:40:29 +0800

    update:更新本地配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
+require("@rushstack/eslint-patch/modern-module-resolution");
+// 保存没有按照 eslint 的规则修复
+// https://segmentfault.com/a/1190000039880312?utm_source=sf-similar-article
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
