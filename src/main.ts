02a1ebe173b97344c89cd75bba1a11f5e33251f8
chendj89 <105529957+chendj89@users.noreply.github.com>
2022-11-29 20:44:16 +0800

    5 files modified

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,32 @@ import App from "./App.vue";
 import router from "./router";
 import "@/scss/app.scss";
 import scss from "@/scss/alias.module.scss";
+console.log(scss);
 import HelloWorld from "./components/HelloWorld.vue";
 const app = createApp(App);
 app.use(router);
 app.component("HelloWorld", HelloWorld);
 app.mount("#app");
+/**
+ * 颜色混合
+ * @param c1 #ffffff
+ * @param c2 #ff5c00
+ * @param ratio 0.0~1.0
+ * @returns 新的颜色
+ */
+const colourBlend = (c1: string, c2: string, ratio: number) => {
+  ratio = Math.max(Math.min(Number(ratio), 1), 0);
+  let r1 = parseInt(c1.substring(1, 3), 16);
+  let g1 = parseInt(c1.substring(3, 5), 16);
+  let b1 = parseInt(c1.substring(5, 7), 16);
+  let r2 = parseInt(c2.substring(1, 3), 16);
+  let g2 = parseInt(c2.substring(3, 5), 16);
+  let b2 = parseInt(c2.substring(5, 7), 16);
+  let r: string | number = Math.round(r1 * (1 - ratio) + r2 * ratio);
+  let g: string | number = Math.round(g1 * (1 - ratio) + g2 * ratio);
+  let b: string | number = Math.round(b1 * (1 - ratio) + b2 * ratio);
+  r = ("0" + (r || 0).toString(16)).slice(-2);
+  g = ("0" + (g || 0).toString(16)).slice(-2);
+  b = ("0" + (b || 0).toString(16)).slice(-2);
+  return "#" + r + g + b;
+};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,13 @@
 
 <template>
   <div class="btn">Primary</div>
-  <div class="btn-1">i</div>
-  <div class="btn-2">i</div>
-  <div class="btn-3">i</div>
-  <div class="btn-4">i</div>
-  <div class="btn-5">i</div>
-  <div class="btn-6">i</div>
-  <div class="btn-7">i</div>
-  <div class="btn-8">i</div>
-  <div class="btn-9">i</div>
+  <div class="btn-1"></div>
+  <div class="btn-2"></div>
+  <div class="btn-3"></div>
+  <div class="btn-4"></div>
+  <div class="btn-5"></div>
+  <div class="btn-6"></div>
+  <div class="btn-7"></div>
+  <div class="btn-8"></div>
+  <div class="btn-9"></div>
 </template>
